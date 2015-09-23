55d0e544e2041f09e39b0b4dfca58519bb1368dc
Ian Billett <ian.billett14@imperial.ac.uk>
2015-09-23 19:40:38 +0100

    adds test for half adder

--- tevol_tests.py
+++ tevol_tests.py
@@ -82,6 +82,42 @@ class TestCircuits(unittest.TestCase):
                                    '10': '1',
                                    '11': '0'})
 
+    def test_half_adder(self):
+        io = IO(2,2)
+        a = Nand()
+        b = Nand()
+        c = Nand()
+        d = Nand()
+        e = Nand()
+        f = Nand()
+        g = Nand()
+        h = Nand()
+        i = Nand()
+
+        io.i[0].connect(a.i[0])
+        io.i[0].connect(b.i[0])
+
+        io.i[1].connect(a.i[1])
+        io.i[1].connect(c.i[1])
+
+        a.o[0].connect(b.i[1])
+        a.o[0].connect(c.i[0])
+
+        b.o[0].connect(d.i[0])
+        c.o[0].connect(d.i[1])
+
+        d.o[0].connect(io.o[0])
+
+        a.o[0].connect(e.i[0])
+        a.o[0].connect(e.i[1])
+
+
+
+        results = io.calc()
+        self.assertEqual(results, {'00': '00',
+                                   '01': '10',
+                                   '10': '10',
+                                   '11': '01'})
 
 if __name__ == '__main__':
     unittest.main()
